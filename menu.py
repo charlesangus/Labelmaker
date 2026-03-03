95f6685e5b2429c8524febc000817cd6638d8ea2
Charles A Taylor <c.angustaylor@gmail.com>
2026-03-03 02:12:38 -0500

    Insert Labelmaker Preferences after Project Settings, move De-overlap to Node Layout submenu

--- menu.py
+++ menu.py
@@ -4,13 +4,22 @@ import labelmaker_deoverlap
 import labelmaker_prefs_dialog
 
 edit_menu = nuke.menu("Nuke").findItem("Edit")
+
+def _find_item_index(parent_menu, item_name):
+    for position, menu_item in enumerate(parent_menu.items()):
+        if menu_item.name() == item_name:
+            return position
+    return -1
+
+project_settings_index = _find_item_index(edit_menu, "Project Settings...")
 edit_menu.addCommand(
     "Labelmaker Preferences...",
     labelmaker_prefs_dialog.show_prefs_dialog,
+    index=project_settings_index + 1,
 )
-edit_menu.addCommand(
+
+node_layout_menu = edit_menu.addMenu("Node Layout")
+node_layout_menu.addCommand(
     "De-overlap All Nodes",
     lambda: labelmaker_deoverlap.deoverlap_all(undoable=True),
 )
-
-nuke.addOnScriptLoad(lambda: labelmaker_deoverlap.deoverlap_all(undoable=True))
